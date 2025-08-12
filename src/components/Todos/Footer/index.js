e70b7c1eedccdb79af98c59dce061700392f26a9
Halit <xltcamm@gmailcom>
2025-08-13 00:31:53 +0200

    alle perfeckt now

---
+++ src/components/Todos/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+
+    return(
+        <div>   
+            FIRST STEP
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Todos/index.js
@@ -0,0 +1,13 @@
+import Section from "./Section";
+import Footer from "./Footer";
+
+function Todos() {
+    return(
+        <div>
+            <Section/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Todos;
